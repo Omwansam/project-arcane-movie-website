256694aae0b11b61a2da0c576fddd391d0c503b4
IanDanCheruiyot <daniank08@gmail.com>
2024-08-14 23:58:31 +0300

    tickets

--- arcane-movie-website/src/components/Posters.js
+++ arcane-movie-website/src/components/Posters.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Posters.css';
 
 function Posters({ title, imageUrl }) {
+  const ticketClick = () => {
+    alert(`You have succesfully purchased a ticket for ${title}!`)
+  };
+
   return (
     <div className="movie-poster">
       <img src={imageUrl} alt={`${title} poster`} />
       <h2>{title}</h2>
+      <button onClick={ticketClick} className='ticket-button'>
+        Ticket
+      </button>
     </div>
   );
 }
