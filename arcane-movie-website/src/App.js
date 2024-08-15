177318ae0ff87d7e0d43da60aa50ebe78881ae94
IanDanCheruiyot <daniank08@gmail.com>
2024-08-15 08:43:24 +0300

    jsx

--- arcane-movie-website/src/App.js
+++ arcane-movie-website/src/App.js
@@ -1,11 +1,13 @@
 // b9cc5a2b  OMBb API KEY
 import './App.css';
 import Navbar from './components/Navbar';
+import Movie from './components/Movie';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Movie/>
     </div>
   );
 }
