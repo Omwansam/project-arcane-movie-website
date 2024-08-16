c84b032afe2f3ba666df9c0b5c88cc908a9b24ab
IanDanCheruiyot <daniank08@gmail.com>
2024-08-16 09:00:09 +0300

    Update App.js

--- arcane-movie-website/src/App.js
+++ arcane-movie-website/src/App.js
@@ -1,15 +1,1 @@
-// b9cc5a2b  OMBb API KEY
-import './App.css';
-import Navbar from './components/Navbar';
-import Movie from './components/Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Movie/>
-    </div>
-  );
-}
-
-export default App;
