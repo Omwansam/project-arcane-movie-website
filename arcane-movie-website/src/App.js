0a48de5f5e70512422abcc7d3dbf62fa5ae1dff5
IanDanCheruiyot <daniank08@gmail.com>
2024-08-16 09:08:09 +0300

    Update App.js

--- arcane-movie-website/src/App.js
+++ arcane-movie-website/src/App.js
@@ -1,1 +1,32 @@
+// // b9cc5a2b  OMBb API KEY
+// import "./App.css";
+// import LoginAndSignUp from "./components/LoginAndSignUp";
+// import Navbar from "./components/Navbar";
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <LoginAndSignUp />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { useLocation, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      {location.pathname !== "/login" && <Navbar />}
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
