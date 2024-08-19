10af6989aefdac2a5ed2907ff21718a7f42b7a8d
jojo <georgebadia2019@gmail.com>
2024-08-19 10:57:14 +0300

    singup page color palet

--- arcane-movie-website/src/index.js
+++ arcane-movie-website/src/index.js
@@ -12,7 +12,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 
